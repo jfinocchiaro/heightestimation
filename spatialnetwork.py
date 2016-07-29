a37ee5300e5c059b2a18e532fb517793430da23e
Jessie Finocchiaro <22jjf22@gmail.com>
2016-07-29 09:04:57 -0400

    Update spatialnetwork.py

--- spatialnetwork.py
+++ spatialnetwork.py
@@ -102,7 +102,7 @@ if __name__ == "__main__":
 
 
     # Test and train model
-    model = spatialNet('spatialwtssmerge.h5')
+    model = spatialNet('spatialwtsmerge.h5')
     opt = SGD(clipvalue=5)
     model.compile(optimizer=opt, loss='mean_squared_error')
     model.fit(trainsamples, y_train_norm, nb_epoch=10, verbose=1)
